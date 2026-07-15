b983efa278aad2d587f4648d70fbf6189f979f83
Eesha <eeshalibrary@gmail.com>
2026-07-15 21:01:40 +0530

    disable eslint exhaustive deps rule

--- src/components/HabitCard.jsx
+++ src/components/HabitCard.jsx
@@ -25,8 +25,8 @@ function HabitCard({ habit, onLogAdded }) {
       })
       .catch((err) => console.error("Failed to fetch logs", err));
   };
+  
 // eslint-disable-next-line react-hooks/exhaustive-deps
-
   useEffect(() => {
     fetchLogs();
   }, [habit.id]);
